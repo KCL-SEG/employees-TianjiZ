59b25afe1c8b1488f652304e1ec8323ab31ab525
TianjiZ <128509320+TianjiZ@users.noreply.github.com>
2023-12-08 01:26:02 +0000

    Add files via upload

--- employee.py
+++ employee.py
@@ -2,59 +2,44 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, base_pay):
+    def __init__(self, name, salary_or_hours, hourly_rate=None):
         self.name = name
-        self.base_pay = base_pay
+        self.salary = salary_or_hours if hourly_rate is None else salary_or_hours * hourly_rate
+        self.hourly_rate = hourly_rate
+        self.commission = 0
+        self.contract_count = 0
 
-    def get_pay(self):
-        return self.base_pay
-
-    def __str__(self):
-        base_str = f"{self.name} works on a base pay of {self.base_pay}."
-        return base_str
-
-class Commission:
-    def __init__(self, amount):
-        self.amount = amount
-
-    def get_commission(self):
-        return self.amount
-
-class ContractCommission(Commission):
-    def __init__(self, contracts, commission_per_contract):
-        super().__init__(contracts * commission_per_contract)
-        self.contracts = contracts
-        self.commission_per_contract = commission_per_contract
-
-    def __str__(self):
-        return f"receives a commission for {self.contracts} contract(s) at {self.commission_per_contract}/contract."
-
-class BonusCommission(Commission):
-    def __init__(self, bonus):
-        super().__init__(bonus)
-        self.bonus = bonus
-
-    def __str__(self):
-        return f"receives a bonus commission of {self.bonus}."
-
-class CommissionedEmployee(Employee):
-    def __init__(self, name, base_pay, commission):
-        super().__init__(name, base_pay)
+    def add_commission(self, commission, contract_count=0):
         self.commission = commission
+        self.contract_count = contract_count
 
     def get_pay(self):
-        return super().get_pay() + self.commission.get_commission()
+        return self.salary + self.commission
 
     def __str__(self):
-        base_str = super().__str__()[:-1]  # Remove the period at the end
-        commission_str = self.commission.__str__()
-        return f"{base_str} and {commission_str}. Their total pay is {self.get_pay()}."
+        if self.hourly_rate is None:
+            base_str = f"{self.name} works on a monthly salary of {self.salary}."
+        else:
+            hours = self.salary // self.hourly_rate
+            base_str = f"{self.name} works on a contract of {hours} hours at {self.hourly_rate}/hour."
+
+        if self.commission > 0:
+            if self.contract_count > 0:
+                commission_str = f" and receives a commission for {self.contract_count} contract(s) at {self.commission // self.contract_count}/contract"
+            else:
+                commission_str = f" and receives a bonus commission of {self.commission}"
+            return f"{base_str}{commission_str}. Their total pay is {self.get_pay()}."
+        else:
+            return f"{base_str} Their total pay is {self.get_pay()}."
 
 # Employee instances
 billie = Employee('Billie', 4000)
-charlie = Employee('Charlie', 2500)  # Assuming Charlie's base pay is pre-calculated
-renee = CommissionedEmployee('Renee', 3000, ContractCommission(4, 200))
-jan = CommissionedEmployee('Jan', 3750, ContractCommission(3, 220))  # Assuming Jan's base pay is pre-calculated
-robbie = CommissionedEmployee('Robbie', 2000, BonusCommission(1500))
-ariel = CommissionedEmployee('Ariel', 3600, BonusCommission(600))  # Assuming Ariel's base pay is pre-calculated
-
+charlie = Employee('Charlie', 100, 25)
+renee = Employee('Renee', 3000)
+renee.add_commission(800, 4)
+jan = Employee('Jan', 150, 25)
+jan.add_commission(660, 3)
+robbie = Employee('Robbie', 2000)
+robbie.add_commission(1500)
+ariel = Employee('Ariel', 120, 30)
+ariel.add_commission(600)
