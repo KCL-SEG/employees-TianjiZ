e10283c6319f2288fe579e82c5189aa5811a0a5c
TianjiZ <128509320+TianjiZ@users.noreply.github.com>
2023-12-08 01:21:35 +0000

    Add files via upload

--- employee.py
+++ employee.py
@@ -2,30 +2,59 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, base_pay):
         self.name = name
+        self.base_pay = base_pay
 
     def get_pay(self):
-        pass
+        return self.base_pay
 
     def __str__(self):
-        return self.name
+        base_str = f"{self.name} works on a base pay of {self.base_pay}."
+        return base_str
 
+class Commission:
+    def __init__(self, amount):
+        self.amount = amount
 
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+    def get_commission(self):
+        return self.amount
 
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+class ContractCommission(Commission):
+    def __init__(self, contracts, commission_per_contract):
+        super().__init__(contracts * commission_per_contract)
+        self.contracts = contracts
+        self.commission_per_contract = commission_per_contract
 
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+    def __str__(self):
+        return f"receives a commission for {self.contracts} contract(s) at {self.commission_per_contract}/contract."
 
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+class BonusCommission(Commission):
+    def __init__(self, bonus):
+        super().__init__(bonus)
+        self.bonus = bonus
 
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+    def __str__(self):
+        return f"receives a bonus commission of {self.bonus}."
 
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+class CommissionedEmployee(Employee):
+    def __init__(self, name, base_pay, commission):
+        super().__init__(name, base_pay)
+        self.commission = commission
+
+    def get_pay(self):
+        return super().get_pay() + self.commission.get_commission()
+
+    def __str__(self):
+        base_str = super().__str__()[:-1]  # Remove the period at the end
+        commission_str = self.commission.__str__()
+        return f"{base_str} and {commission_str}. Their total pay is {self.get_pay()}."
+
+# Employee instances
+billie = Employee('Billie', 4000)
+charlie = Employee('Charlie', 2500)  # Assuming Charlie's base pay is pre-calculated
+renee = CommissionedEmployee('Renee', 3000, ContractCommission(4, 200))
+jan = CommissionedEmployee('Jan', 3750, ContractCommission(3, 220))  # Assuming Jan's base pay is pre-calculated
+robbie = CommissionedEmployee('Robbie', 2000, BonusCommission(1500))
+ariel = CommissionedEmployee('Ariel', 3600, BonusCommission(600))  # Assuming Ariel's base pay is pre-calculated
+
